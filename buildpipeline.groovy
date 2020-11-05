655b942cc654af0c2f7d02fc93d299933c527665
Dinesh Baria <dinesh.baria@microchip.com>
2020-11-05 12:34:27 +0530

    github stage  check

--- buildpipeline.groovy
+++ buildpipeline.groovy
@@ -15,9 +15,14 @@ def runStages() {
 	}
 	stage('github-deploy') {
 		if(env.BRANCH_NAME == 'test') {
-			echo "GitHub deploy"
-			echo "${params.NOTIFICATION_EMAIL} ${env.BITBUCKET_URL}"
-			execute("exit 1")
+			def githubObj = getGiHubInfo()					
+			download("tool-github-deploy","1.0.0")		
+			execute("chmod +x ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py")  
+					
+			withCredentials([usernamePassword(credentialsId: '8bit-examples.github.com', usernameVariable: 'USER_NAME', passwordVariable:'USER_PASS' )]) {					
+				execute("python ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py -deploy=true -gpat=${USER_PASS} -dgid=${USER_NAME} -dburl=${env.BITBUCKET_URL} -dgurl=${env.GITHUB_URL} -dtag=${env.TAG_NAME} -dmfd=true")						
+				execute("python ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py -rlo=true -gpat=${USER_PASS}  -rpn=${githubObj.repoName} -rltv=${env.TAG_NAME} -rltt=${env.TAG_NAME} -dmfd=true")	
+			}
 		}		
 	}
 	stage('portal-deploy') {
@@ -36,5 +41,23 @@ def execute(String cmd) {
 	} else {
 		bat cmd
 	}
+}
+def download(String toolName,String toolVersion) {
+	def repo = "ivy/citd"
+	def url = "${env.ARTIFACTORY_SERVER}/${repo}/${toolName}/${toolVersion}/${toolName}-${toolVersion}.zip"
+	def response =sh(script:"curl ${url} -o ${toolName}.zip",returnStdout: true).trim()
+	unzip dir:"${toolName}", quiet: true, zipFile: "${toolName}.zip"	
+	execute("rm -rf ${toolName}.zip")
+}
+def getGiHubInfo() {
+	def githubObj = [
+		'ownerName':'',
+		'repoName':''
+		]
+	String[] splitURLString = "${env.GITHUB_URL}".split("/")
+	githubObj.repoName = splitURLString[splitURLString.size()-1]
+	githubObj.repoName = githubObj.repoName.replace(".git","")
+	githubObj.ownerName = splitURLString[splitURLString.size()-2]
+	return githubObj
 }
 return this
