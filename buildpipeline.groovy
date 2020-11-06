3309e44bc22dc53b5fb7d7bc9848ec4c3fcd9ff7
Dinesh Baria <dinesh.baria@microchip.com>
2020-11-06 08:39:21 +0530

    email method check

--- buildpipeline.groovy
+++ buildpipeline.groovy
@@ -1,35 +1,38 @@
 def runStages() {
-	try {
-	stage('pre-build'){
-		def mplabxPath= sh (script: 'update-alternatives --list MPLABX_PATH',returnStdout: true).trim()
-		def compilerPath= sh (script: 'update-alternatives --list XC8_PATH',returnStdout: true).trim()										
-		def pDir = "${mplabxPath}/packs"				
-		def ver = compilerPath.split('/')[4].substring(1)		
-		
-		download("tool-mplabx-c-project-generator","1.0.0")					
-		execute("cd tool-mplabx-c-project-generator && node configGenerator.js sp=../ v8=${ver} packs=${pDir}")	
-	}
-	stage('build') {
-		download("tool-mplabx-c-build","1.0.0")								
-		execute("cd tool-mplabx-c-build && node buildLauncher.js sp=../ rp=./output genMK=true")
-	}
-	stage('github-deploy') {
-		if(env.TAG_NAME =~ env.SEMVER_REGEX) {
-			def githubObj = getGiHubInfo()					
-			download("tool-github-deploy","1.0.0")		
-			execute("chmod +x ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py")  
-					
-			withCredentials([usernamePassword(credentialsId: 'BD1085_GitHub_Token', usernameVariable: 'USER_NAME', passwordVariable:'USER_PASS' )]) {					
-				execute("python ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py -deploy=true -gpat=${USER_PASS} -dgid=${USER_NAME} -dburl=${env.BITBUCKET_URL} -dgurl=${env.GITHUB_URL} -dtag=${env.TAG_NAME} -dmfd=true")						
-				execute("python ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py -rlo=true -gpat=${USER_PASS}  -rpn=${githubObj.repoName} -rltv=${env.TAG_NAME} -rltt=${env.TAG_NAME} -dmfd=true")	
-			}
-		}		
-	}
-	stage('portal-deploy') {
-		if(env.TAG_NAME =~ env.SEMVER_REGEX){
-			echo "Portal deploy"
+	try {	
+		stage('pre-build'){
+			def mplabxPath= sh (script: 'update-alternatives --list MPLABX_PATH',returnStdout: true).trim()
+			def compilerPath= sh (script: 'update-alternatives --list XC8_PATH',returnStdout: true).trim()										
+			def pDir = "${mplabxPath}/packs"				
+			def ver = compilerPath.split('/')[4].substring(1)		
+			
+			download("tool-mplabx-c-project-generator","1.0.0")					
+			execute("cd tool-mplabx-c-project-generator && node configGenerator.js sp=../ v8=${ver} packs=${pDir}")	
 		}
-	}
+		
+		stage('build') {
+			download("tool-mplabx-c-build","1.0.0")								
+			execute("cd tool-mplabx-c-build && node buildLauncher.js sp=../ rp=./output genMK=true")
+		}
+		
+		stage('github-deploy') {
+			if(env.TAG_NAME =~ env.SEMVER_REGEX) {
+				def githubObj = getGiHubInfo()					
+				download("tool-github-deploy","1.0.0")		
+				execute("chmod +x ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py")  
+						
+				withCredentials([usernamePassword(credentialsId: 'BD1085_GitHub_Token', usernameVariable: 'USER_NAME', passwordVariable:'USER_PASS' )]) {					
+					execute("python ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py -deploy=true -gpat=${USER_PASS} -dgid=${USER_NAME} -dburl=${env.BITBUCKET_URL} -dgurl=${env.GITHUB_URL} -dtag=${env.TAG_NAME} -dmfd=true")						
+					execute("python ./tool-github-deploy/tool-github-deploy/tool-github-deploy.py -rlo=true -gpat=${USER_PASS}  -rpn=${githubObj.repoName} -rltv=${env.TAG_NAME} -rltt=${env.TAG_NAME} -dmfd=true")	
+				}
+			}		
+		}
+		
+		stage('portal-deploy') {
+			if(env.TAG_NAME =~ env.SEMVER_REGEX){
+				echo "Portal deploy"
+			}
+		}	
 	} finally {
 		// Archive the build output artifacts.
 		archiveArtifacts artifacts: "tool-mplabx-c-build/output/**", allowEmptyArchive: true, fingerprint: true
@@ -68,7 +71,7 @@ def getGiHubInfo() {
 }
 
 def sendPipelineFailureEmail () {			  
-    mail to: "${params.NOTIFICATION_EMAIL}",
+    emailext to: "${params.NOTIFICATION_EMAIL}",
     subject: "Failed Pipeline: ${currentBuild.fullDisplayName}",
     body: "Pipeline failure. ${env.BUILD_URL}"
 }
